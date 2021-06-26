e87f816339496d10718686af01f497871bd81f21
Janell Myrick <76677736+luvthatduck@users.noreply.github.com>
2021-06-26 12:35:13 -0600

    trying to fix heroku issue

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,15 @@
 const Sequelize = require('sequelize');
 
+
 require('dotenv').config();
 
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+if (process.env.CLEARDB_DATABASE_URL) {
+  sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL);
 } else {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
+    host: process.env.DB_HOST,
     dialect: 'mysql',
     port: 3306
   });
